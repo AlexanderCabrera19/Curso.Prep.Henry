ca420e170cf5f099b514c150e1b3ed540488b99e
Alexander Cabrera <alexander.cabrera.2011@gmail.com>
2021-12-21 00:47:35 -0300

    02-JS-I-homework

---
+++ 02-JS-I/homework/conceptos.js
@@ -0,0 +1,16 @@
+/*
+VARIABLE:
+Es un espacio reservado de memoria, para almacenar valor de algo que va a ser usado y, así mismo, pueden ser modificado a lo largo del tiempo. 
+
+STRING:
+Es una cadena de texto, que siempre será definida entre comillas (simples o dobles), para que de esta manera todo lo que esté contenido entre las comillas sea considerado un string.
+
+FUNCIONES + RETURN:
+Una función es un conjunto de intrucciones que realiza una tarea específica dentro de nuestro programa, que debe de tener una entrada y una salida. La salida, propiamente dicha, es conocida como "return", y es la única forma en que los datos escapan de una función.
+
+DECLARACIONES (if):
+Son sentencias condicionales que está compuesta por instrucciones que pueden ser ejectuadas, o no, dependiendo del valor de verdad de la condición.
+
+VALORES BOOLEANOS
+Representan valores de verdad, es decir TRUE o False. 
+*/ 
